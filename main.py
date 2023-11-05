4d965ae80076cd1ec9fa85f69401b13b733af902
Rah <arrehman757@gmail.com>
2023-11-05 19:42:42 +0530

    - added showResources() to show the 'report' of the machine ingredients remaining - created startGame() in order to start the game at the end of the program - so all functions can be defined before execution

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import data
 
-stillInGame = True
+def startGame(stillInGame):
+    while stillInGame == True:
+        order = input("What would you like? (espresso/latte/cappuccino):").lower()
+        if order in data.menu:
+            print(f"You have ordered a {order}")
+            print(f"The drink costs {data.menu[order]['cost']}$")
+            print("Please enter your coins: ")
+            # checkResources()
+            # insertCoins()
+            # makeCoffee()
+            print(f"Here is your {order}, Enjoy!")
 
-while stillInGame == True:
-    order = input("What would you like? (espresso/latte/cappuccino):").lower()
-    if order in data.menu:
-        print(f"You have ordered a {order}")
-        print(f"The drink costs {data.menu[order]['cost']}$")
-        print("Please enter your coins: ")
-        # checkResources()
-        # insertCoins()
-        # makeCoffee()
-        print(f"Here is your {order}, Enjoy!")
-
-    elif order == "off":
-        print("The machine is turning off... ")
-        stillInGame = False
-        break;
-    elif order == "report":
-        print("Printing machine report: \n")
-#        showResources()             #shows all resources.
-    else: 
-        print("Enter a proper input.")
-
+        elif order == "off":
+            print("The machine is turning off... ")
+            stillInGame = False
+            break;
+        elif order == "report":
+            print("Printing machine report: \n")
+            showResources()             #shows all resources.
+        else: 
+            print("Enter a proper input.")
 
 # def checkResources():
 
@@ -30,4 +28,14 @@ while stillInGame == True:
 
 # def makeCoffee():              # reduce the coffee resources from total resources
 
-# def showResources():
+def showResources():
+    print("----- Resources left in stock -----")
+    for element in data.resources:
+        print(f"{element} = {data.resources[element]}   ",end='')
+    print("\n")
+
+
+
+stillInGame = True                       # make it true to start game 
+
+startGame(stillInGame)                   # Start game at the end of the program so that all the functions get defined first.
