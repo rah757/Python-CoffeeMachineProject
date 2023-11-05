b3565f183787aa5541a3ac62f8c9e78e9ed9be51
Rah <arrehman757@gmail.com>
2023-11-05 17:17:09 +0530

    added all basic functions for implementation

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import data
 
-StillInGame = True
+stillInGame = True
 
-
-while StillInGame == True:
+while stillInGame == True:
     order = input("What would you like? (espresso/latte/cappuccino):").lower()
     if order in data.menu:
-        print(f"you have ordered a {data.menu[order]}")
+        print(f"You have ordered a {order}")
+        print(f"The drink costs {data.menu[order]['cost']}$")
+        print("Please enter your coins: ")
+        # checkResources()
+        # insertCoins()
+        # makeCoffee()
+        print(f"Here is your {order}, Enjoy!")
+
     elif order == "off":
         print("The machine is turning off... ")
-        StillInGame = False
+        stillInGame = False
         break;
     elif order == "report":
         print("Printing machine report: \n")
 #        showResources()             #shows all resources.
     else: 
         print("Enter a proper input.")
+
+
+# def checkResources():
+
+# def insertCoins():              #insert coins and calc total - check if tx was successful - if extra money, provide change
+
+# def makeCoffee():              # reduce the coffee resources from total resources
+
+# def showResources():
