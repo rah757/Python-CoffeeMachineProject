502dcfdc84bcc929e78765b7c24affc525268a24
Rah <arrehman757@gmail.com>
2023-11-06 23:50:20 +0530

    added insertCoins() and makeCoffee()
    organized the code.
    insertCoins()
    - uses dictionary to save coin values
    - finds total amount and calculates if its enough
    makeCoffee()
    - reduces required resources from total resources

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 import data
 
-
-
 def startGame(stillInGame):
     while stillInGame == True:
-        order = input("What would you like? (espresso/latte/cappuccino):").lower()
+        order = input("\nWhat would you like? (espresso/latte/cappuccino):").lower()
         if order in data.menu:
-            print(f"You have ordered a {order}")
-            print(f"The drink costs {data.menu[order]['cost']}$")
-            print("Please enter your coins: ")
-            print("\n \n \n \n \n ")
-
+            print(f"\nYou have ordered a {order}")
+            print(f"\nThe drink costs {data.menu[order]['cost']}$")
+            print("\nPlease insert your coins: ")
             checkResources(order)        # testing
-
-            print("\n \n \n \n \n ")
-            # insertCoins()
-            # makeCoffee()
-            print(f"Here is your {order}, Enjoy!")
+            insertCoins(order)
+            makeCoffee(order)
+            print(f"\nHere is your {order}, Enjoy!")
 
         elif order == "off":
-            print("The machine is turning off... ")
+            print("\nThe machine is turning off...  ")
             stillInGame = False
             break;
+        
         elif order == "report":
-            print("Printing machine report: \n")
+            print("\nPrinting machine report:\n \n")
             showResources()             #shows all resources.
+
         else: 
-            print("Enter a proper input.")
+            print("\nEnter a proper input.")
+
+        if input("\nwould you like to order another coffee? (y/n): ") == 'n':
+            stillInGame = False
+            print("\nHope you enjoyed your order, please visit again soon! \n")
+        else:
+            stillInGame = True
+
 
 def ingredientFinder(order,item):
     return data.menu[order]['ingredients'][item], data.resources[item]
@@ -37,8 +40,8 @@ def checkResources(order):
     coffee, resourceCoffee = ingredientFinder(order, 'coffee')
     requirements = [water, milk, coffee]
     resources = [resourceWater, resourceMilk, resourceCoffee]
-
     enough = True
+
     for i in range(0,len(resources)):
         if requirements[i] <= resources[i]:
             enough = True
@@ -46,15 +49,32 @@ def checkResources(order):
             enough = False
             break;
     
-    if enough == True:
-        print("yes u got cofi")             #testing
+    if enough == False:
+        print("Not enough resources in coffee machine, please visit some other time.")
+        stillInGame = False
+        return
 
-    
 
+def insertCoins(order):              #insert coins and calc total - check if tx was successful - if extra money, provide change
+    values = {
+        'quarter':0.25,
+        'dime':0.1,
+        'nickel':0.05,
+        'penny':0.01,
+    }
+    total = 0
+    for element in values:
+        total += int(input(f"Enter the number of {element}: ")) * values[element]
+    if total >= data.menu[order]['cost']:
+        print(f"\nYour change is {total - data.menu[order]['cost']}$")
+        makeCoffee(order)
+    else:
+        print("not enough money")
+        return
 
-# def insertCoins():              #insert coins and calc total - check if tx was successful - if extra money, provide change
-
-# def makeCoffee():              # reduce the coffee resources from total resources
+def makeCoffee(order):              # reduce the coffee resources from total resources
+    for element in data.resources:
+        data.resources[element] -= data.menu[order]['cost']
 
 def showResources():
     print("----- Resources left in stock -----")
@@ -63,7 +83,5 @@ def showResources():
     print("\n")
 
 
-
 stillInGame = True                       # make it true to start game 
-
 startGame(stillInGame)                   # Start game at the end of the program so that all the functions get defined first.
